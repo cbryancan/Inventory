587d4d817183c5407d3532b63a65eb55d8d53c88
cbryancan <cbryancan@gmail.com>
2017-01-18 13:08:59 -0500

    Sell button bugs fixed

--- app/src/main/java/com/cbryancan/inventory/ProductCursorAdapter.java
+++ app/src/main/java/com/cbryancan/inventory/ProductCursorAdapter.java
@@ -17,7 +17,6 @@ import static com.cbryancan.inventory.R.id.quantity;
 
 public class ProductCursorAdapter extends CursorAdapter {
 
-    int productQuantity;
 
     public ProductCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
@@ -29,22 +28,25 @@ public class ProductCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
+
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
         TextView quantityTextView = (TextView) view.findViewById(quantity);
         TextView priceTextView = (TextView) view.findViewById(R.id.price);
         TextView saleTextView = (TextView) view.findViewById(R.id.sale);
         Button sellButton = (Button) view.findViewById(R.id.sell_item);
-        String position = String.valueOf(getCursor().getPosition());
-        final Uri currentUri = Uri.withAppendedPath(InventoryContract.ProductEntry.CONTENT_URI, position);
+        int cursor_id = cursor.getInt(cursor.getColumnIndex(InventoryContract.ProductEntry._ID));
+        final Uri currentUri = Uri.withAppendedPath(InventoryContract.ProductEntry.CONTENT_URI, String.valueOf(cursor_id));
 
+        final int idColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry._ID);
         int nameColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME);
         int quantityColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
         int priceColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry.COLUMN_PRODUCT_PRICE);
         int saleColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry.COLUMN_PRODUCT_SALE);
 
+        final int productQuantity = cursor.getInt(quantityColumnIndex);
+
         String productName = cursor.getString(nameColumnIndex);
-        productQuantity = cursor.getInt(quantityColumnIndex);
         String productPrice = cursor.getString(priceColumnIndex);
         int productSale = cursor.getInt(saleColumnIndex);
         String saleString;
@@ -62,20 +64,17 @@ public class ProductCursorAdapter extends CursorAdapter {
         sellButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                decrementQuantity(view.getContext(), currentUri);
-            }
+                cursor.moveToPosition(idColumnIndex);
+                if (productQuantity > 0) {
+                    ContentValues cv = new ContentValues();
+                    int newItemQuantity = productQuantity - 1;
+                    cv.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, newItemQuantity);
+                    view.getContext().getContentResolver().update(currentUri, cv, null, null);
+                } else {
+                    return;
+                }            }
         });
 
     }
 
-    private void decrementQuantity(Context context, Uri currentUri) {
-        if (productQuantity >= 0) {
-            ContentValues cv = new ContentValues();
-            productQuantity --;
-            cv.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, productQuantity);
-            context.getContentResolver().update(currentUri, cv, null, null);
-        } else {
-            return;
-        }
-    }
 }
